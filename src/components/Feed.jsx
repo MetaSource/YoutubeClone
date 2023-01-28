1b6cc1eed65bec89aa10c9f4722710f983148924
MetaSource <jsal91@gmail.com>
2023-01-28 02:35:13 -0800

    implement Sidebar component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import {Box, Stack, Typography} from '@mui/material'
+import Sidebar from './Sidebar'
+
 
 
 const Feed = () => {
@@ -10,7 +12,7 @@ const Feed = () => {
                 height: {sx: 'auto', md: '92vh'}, 
                 borderRight: '1px solid #3d3d3d', 
                 px : {sx: 0, md:2}}}>
-            SideBar   
+            <Sidebar/>   
             <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
                 Copyright 2022 STRMLIND
             </Typography>
