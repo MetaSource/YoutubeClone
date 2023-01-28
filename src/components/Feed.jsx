83080a272b025b2e9308f47d708dd9aab410522d
MetaSource <jsal91@gmail.com>
2023-01-28 04:25:11 -0800

    Update styling in feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -16,6 +16,12 @@ const Feed = () => {
             <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
                 Copyright 2022 STRMLIND
             </Typography>
+        </Box >
+            
+        <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white'}}>
+                New <span style={{color: '#F31503'}}>Videos</span>
+            </Typography>
         </Box>
     </Stack>
   )
