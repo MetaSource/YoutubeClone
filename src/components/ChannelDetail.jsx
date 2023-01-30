0ed89a01b562e8243653ee06172d8919ae053a0d
MetaSource <jsal91@gmail.com>
2023-01-30 06:29:36 -0800

    Implement channel detail components and make channelCard resuable

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { useState, useEffect}  from 'react'
+import { useParams } from 'react-router-dom'
+import { Box} from '@mui/material'
+import {Videos, ChannelCard} from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetail = () => {
+
+  const [channelDetail, setChannelDetail] = useState()
+  const [videos, setVideos] = useState([])
+  const { id } = useParams();
+
+  console.log({id});
+  console.log(channelDetail, videos);
+
+  useEffect( () => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then( (data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then( (data) => setVideos(data?.items));
+
+  }, [id]);
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+         <div style={{
+           background: 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,45,229,0.9880005926148504) 100%)',
+           zIndex: 10, 
+           height: '300px'
+          }}
+          />
+          <ChannelCard channelDetail={channelDetail} marginTop="-110px"/>
+      </Box>
+      <Box>
+        <Box display="flex" p="2">
+          <Box sx={{mr: {sm: '100px'}}}/>
+          <Videos videos={videos}/>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
